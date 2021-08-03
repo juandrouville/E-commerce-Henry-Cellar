188f7e48306cc5d5eb3a6fe21d1216c15682f82c
horaciodemaio <horaciodemaio33@gmail.com>
2021-08-03 12:04:17 -0300

    api/ cree archivo index.js con el puerto 3001

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+
+
+// Syncing all the models at once.
+conn.sync({ force: true })
+.then(() => {
+    console.log("sincroniza servidor 3001")
+  server.listen(3001, () => {
+    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  });
+});
