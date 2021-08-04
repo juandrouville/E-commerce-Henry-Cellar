246adfa9282a10644d1fcec234077b2fb4816f99
Juan Jose Peralta <jperalta@pixel2html.net>
2021-08-04 11:51:32 -0300

    agregue componente product y productDetail

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,5 +1,12 @@
-const Product = ({ name, image, price, stock }) => {
-  return <div></div>;
+const Product = ({ stock, image, name, price }) => {
+  return (
+    <div className="product">
+      <p>{stock}</p>
+      <img src={image} alt="Henry" />
+      <h3>{name}</h3>
+      <p>{price}</p>
+    </div>
+  );
 };
 
 export default Product;
