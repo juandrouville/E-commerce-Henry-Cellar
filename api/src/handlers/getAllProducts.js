61c422d56fdcc2dfa2d49d0f96a1a4c3c83b5d9f
horaciodemaio <horaciodemaio33@gmail.com>
2021-08-04 12:24:21 -0300

    api/src/handlers/getallproducts cree ruta para traer todos los productos

--- api/src/handlers/getAllProducts.js
+++ api/src/handlers/getAllProducts.js
@@ -1,13 +1,40 @@
-const {Recipe, Diet} = require("../db");
-const axios = require("axios");
+const { Product, Categories } = require("../db");
+//const axios = require("axios");
 
-const getAllproducts = async (req, res, next)=>{
+const getAllproducts = async (req, res, next) => {
     var name = req.query.name;
     var precio = req.query.precio;
     var categoria = req.query.categoria;
     var bodega = req.query.bodega
+    try {
+        const ProductDB = await Product.findAll({
+           
+            include: {
+                model: Categories,
+                attributes: ["name", "varietal", "type"],
+                through: {
+                    attributes: []
+                }
+            }
+        });
 
-    
+        const resultAllProduct = [];
+        ProductDB.forEach((p)=>{
+            let obj = {
+                name: p.name,
+                img: p.img,
+                description: p.description,
+                bodega: p.bodega,
+                precio: p.precio
+            }
+            resultAllProduct.push(obj);
+        })
+
+        res.send(resultAllProduct);
+
+    } catch (error) {
+        next(error)
+    }
 
 
 }
@@ -15,5 +42,5 @@ const getAllproducts = async (req, res, next)=>{
 
 module.exports = {
     getAllproducts,
-    
+
 };
