82e791dac3fdf2d64972e26191b914372ac5cdcf
horaciodemaio <horaciodemaio33@gmail.com>
2021-08-04 15:37:34 -0300

    api/ modifique ruta de getallproducts

--- api/src/handlers/getAllProducts.js
+++ api/src/handlers/getAllProducts.js
@@ -7,12 +7,12 @@ const getAllproducts = async (req, res, next) => {
     var categoria = req.query.categoria;
     var bodega = req.query.bodega
     if (order) {
-        if (precio === 'Ascendant'){
+        if (precio === 'Ascendant') {
             var asc = await Prodcut.findAll({
                 order: sequelize.literal('precio ASC')
             })
             res.send(asc)
-        } 
+        }
         if (precio === 'Descendant') {
             var desc = await Prodcut.findAll({
                 order: sequelize.literal('precio DESC')
@@ -25,41 +25,40 @@ const getAllproducts = async (req, res, next) => {
                     categoria: categoria,
                 }
             })
-            if(findOne.length === 0){
+            if (findOne.length === 0) {
                 return res.status(404).send('Error: Name of continent is invalid')
             } else return res.json(findOne)
         }
     }
-    else{
-    try {
-        const ProductDB = await Product.findAll({
-           
-            include: {
-                model: Categories,
-                attributes: ["name", "varietal", "type"],
-                through: {
-                    attributes: []
+    else {
+        try {
+            const ProductDB = await Product.findAll({
+
+                include: {
+                    model: Categories,
+                    attributes: ["name", "varietal", "type"],
+                    through: {
+                        attributes: []
+                    }
                 }
-            }
-        });
+            });
 
-        const resultAllProduct = [];
-        ProductDB.forEach((p)=>{
-            let obj = {
-                name: p.name,
-                img: p.img,
-                description: p.description,
-                harvest: p.harvest,
-                precio: p.precio
-            }
-            resultAllProduct.push(obj);
-        })
+            const resultAllProduct = [];
+            ProductDB.forEach((p) => {
+                let obj = {
+                    name: p.name,
+                    image: p.image,
+                    price: p.price,
+                    stock: p.stock
+                }
+                resultAllProduct.push(obj);
+            })
 
-        res.send(resultAllProduct);
+            res.send(resultAllProduct);
 
-    } catch (error) {
-        next(error)
-    }
+        } catch (error) {
+            next(error)
+        }
 
     }
 }
