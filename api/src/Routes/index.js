b2407bc8cb527dc3f78bf30d11377f2eddb3ccef
juandrouville <juandrouville>
2021-08-04 14:09:40 -0300

    Api/acepte conflicto en product detail

--- api/src/Routes/index.js
+++ api/src/Routes/index.js
@@ -1,18 +1,11 @@
 const { Router } = require('express');
 const router = Router();
-<<<<<<< HEAD
-const getAllProductsRouter = require("./getAllProducts");
-
-
-router.use("/allproducts", getAllProductsRouter);
-=======
-const productsRouter = require("./products");
+// const productsRouter = require("./products");
 const productDetail=require("./productDetail")
 
 
-router.use("/products", productsRouter);
+// router.use("/products", productsRouter);
 router.use("/product",productDetail)
->>>>>>> fede
 
 
 
