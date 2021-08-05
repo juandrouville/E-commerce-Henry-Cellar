8c7262840938f74537768205398bb80b0e9fecb9
fedecontegrand <fede.contegrand@gmail.com>
2021-08-05 18:11:19 -0300

    back de edit agregado, front en proceso

---
+++ client/src/components/Edit/Edit.js
@@ -0,0 +1,17 @@
+import React from 'react'
+//import axios from 'axios'
+
+export default function Edit({id}) {
+
+    // useEffect con axios.get en la ruta del ProductDetail asi traigo todo los detalles ya existentes
+
+    // onSubmit este form hace un PUT a la ruta editProduct con los valores que cambio
+
+    // FORM hacer con react-hook-form
+
+    return (
+        <div>
+            form to edit existent product
+        </div>
+    )
+}
