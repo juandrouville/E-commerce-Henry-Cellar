452fe5f620add63887caa4dd0f351c96f3860dec
lighuen <lighu.miranda@gmail.com>
2021-08-04 23:42:52 -0300

    client/ Bases react-redux y navbar base hecha

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,34 +1,32 @@
-
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { getNameProduct } from '../../actions';
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+// import { getNameProduct } from '../../actions';
 
 const SearchBar = () => {
-    const dispatch = useDispatch();
-    const [search,setSearch] = useState("");
-    
-    const handleInputChange = (event) =>{
-        event.preventDefault();
-      setSearch(event.target.value);
-    }
+  const dispatch = useDispatch();
+  const [search, setSearch] = useState("");
 
-    const handleClick = (event)=>{
-        event.preventDefault();
-        //dispatch(getNameProduct(product));
+  const handleInputChange = (event) => {
+    event.preventDefault();
+    setSearch(event.target.value);
+  };
 
-    }
-    
-    return (
-        <div class="search-box">
-            <input class="search-txt"
-            type="search"
-            placeholder="Search for products"
-            onChange={(e)=> handleInputChange(e)}/>
-            <button onClick={(e)=> handleClick(e)}>Find</button>
-            
-        </div>
-        
-    )
-}
+  const handleClick = (event) => {
+    event.preventDefault();
+    //dispatch(getNameProduct(product));
+  };
+
+  return (
+    <div class="search-box">
+      <input
+        class="search-txt"
+        type="search"
+        placeholder="Search for products"
+        onChange={(e) => handleInputChange(e)}
+      />
+      <button onClick={(e) => handleClick(e)}>Find</button>
+    </div>
+  );
+};
 
 export default SearchBar;
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,11 +1,13 @@
+import menu from "../components/menu/index.js";
+
 const Home = () => {
   return (
     <div className="welcome-screen">
       <div>
-        
+        <menu />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
