8a3842ecd3187b8d7fa62e446802d904e72b8aff
juandrouville <juandrouville>
2021-08-06 17:42:18 -0300

    detalle en set detail

--- api/src/initialContents/SetDataInitial.js
+++ api/src/initialContents/SetDataInitial.js
@@ -1,5 +1,5 @@
 
-const { Product, Categories,Wineries } = require("../db");
+const { Product, Categories, Wineries } = require("../db");
 
 async function SetDataInitial(arrayProducts) {
   try {
@@ -18,20 +18,20 @@ async function SetDataInitial(arrayProducts) {
       var oneProduct = await Product.findOne({ where: { name: arrayProducts[e].name } });
 
 
-      
+
       await arrayProducts[e].category.map(async (e) => {
         await Categories.findOrCreate({ where: { name: e } });
         var category = await Categories.findOne({ where: { name: e } });
         await oneProduct.addCategories(category);
       })
-      
-      if(arrayProducts[e].bodega){
-        
+
+      if (arrayProducts[e].bodega) {
+
         await Wineries.findOrCreate({ where: { name: arrayProducts[e].bodega } });
         var winerie = await Wineries.findOne({ where: { name: arrayProducts[e].bodega } });
         await winerie.setProducts(oneProduct);
       }
-      
+
     };
 
   } catch (error) {
