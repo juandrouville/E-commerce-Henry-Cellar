ede4aab0a35f7bf8f4803dd3ea5b42a768b45ab9
lighuen <lighu.miranda@gmail.com>
2021-08-06 01:51:18 -0300

    Sequelize error arreglado / imágenes del front arregladas / navbar borrado de las rutas del front

--- client/src/components/product/Product.js
+++ client/src/components/product/Product.js
@@ -2,19 +2,16 @@ import React from "react";
 
 const Product = ({ image, name, price }) => {
   return (
-    <div className="product">
-      <img
-        src={`${image}`}
-        alt="Henry"
-        width="246"
-        height="246"
-      />
-      <div className="product__data">
-        <h3>{name}</h3>
-        <br />
-        <p>{price}</p>
-        <br />
-        <p className="product__stock">Stock</p>
+    <div className="row center">
+      <div key={name} className="product">
+        <img src={`${image}`} alt="Henry" width="246" height="246" />
+        <div className="product__data">
+          <h3>{name}</h3>
+          <br />
+          <p>{price}</p>
+          <br />
+          <p className="product__stock">Stock</p>
+        </div>
       </div>
     </div>
   );
