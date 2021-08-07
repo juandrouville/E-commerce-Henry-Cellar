92f1ddeb4ecfe6bee810c93fe99e1fcef197ad7e
Juan Jose Peralta <jperalta@pixel2html.net>
2021-08-07 16:37:42 -0300

    General /arregle la estructura donde tiene que ir cada componente en el front

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,21 @@
 import NavBar from "../components/NavBar/NavBar.js";
 import AllProducts from "../components/allProducts/allproducts";
-import Wine from "assets/images/backgrond-wine.jpeg";
 
-const Catalogo = () => {
+import Wine from "assets/images/backgrond-wine.jpeg";
+import Layout from "layouts/layout-primary";
+
+const Home = () => {
   return (
-    <div>
-      <div>
-        <NavBar />
+    <Layout>
+      <div className="catalogo__container">
+        <img src={Wine} alt="Henry" width="100%" />
+        <div>
+          <h1 className="catalogo__title">Catalogo</h1>
+          <AllProducts />
+        </div>
       </div>
-      <div className="container">
-        <AllProducts />
-      </div>
-    </div>
+    </Layout>
   );
 };
 
-export default Catalogo;
+export default Home;
