3f673122793fc0ebed9a17a0186a52cb21437f17
Lighuen Miranda <77689041+Salvapantallado@users.noreply.github.com>
2021-08-09 14:18:00 -0300

    Categorias postform con select

--- client/src/pages/PostForm.js
+++ client/src/pages/PostForm.js
@@ -79,18 +79,17 @@ export default function PostProduct() {
 
   return (
     <Layout>
-      <div>
-        <Link to={`/`}>
-          <button>home</button>
-        </Link>
-        <div className="form">
-          <img
-            src="https://previews.123rf.com/images/sakmeniko/sakmeniko2007/sakmeniko200700116/150774024-water-bottle-icon-vector-design-template.jpg"
-            alt="post bottle"
-          />
-          <form className="table" onSubmit={handleSubmit}>
+     <div className="form__container">
+      <NavBar />
+      <Link to={`/`}>
+        <button>home</button>
+      </Link>
+      <div className="form">
+        <img src={wineimage} alt="post wine" width="50%" />
+        <form className="table" onSubmit={handleSubmit}>
+          <div className="form__inputs">
             <div>
-              <label>Product name:</label>
+              <label>Product name</label>
               <input
                 className={errors.name && "danger"}
                 type="text"
@@ -99,88 +98,7 @@ export default function PostProduct() {
                 value={input.name}
               />
               {errors.name && <p className="danger">{errors.name}</p>}
-              <div>
-                <label>Categoría:</label>
-                <input
-                  className={errors.categoria && "danger"}
-                  type="text"
-                  name="categoria"
-                  onChange={handleInputChange}
-                  value={input.categoria}
-                />
-                {errors.categoria && (
-                  <p className="danger">{errors.categoria}</p>
-                )}
-                <div>
-                  <label>Bodega:</label>
-                  <input
-                    className={errors.bodega && "danger"}
-                    type="text"
-                    name="bodega"
-                    onChange={handleInputChange}
-                    value={input.bodega}
-                  />
-                  {errors.bodega && <p className="danger">{errors.bodega}</p>}
-                  <div>
-                    <label>Cosecha:</label>
-                    <input
-                      className={errors.harvest && "danger"}
-                      type="text"
-                      name="harvest"
-                      onChange={handleInputChange}
-                      value={input.harvest}
-                    />
-                    {errors.harvest && (
-                      <p className="danger">{errors.harvest}</p>
-                    )}
-                    <div>
-                      <label>Precio:</label>
-                      <input
-                        className={errors.price && "danger"}
-                        type="number"
-                        name="price"
-                        onChange={handleInputChange}
-                        value={input.price}
-                      />
-                      {errors.price && <p className="danger">{errors.price}</p>}
-                      <div>
-                        <label>Descripción:</label>
-                        <input
-                          className={errors.description && "danger"}
-                          type="text"
-                          name="description"
-                          onChange={handleInputChange}
-                          value={input.description}
-                        />
-                        {errors.description && (
-                          <p className="danger">{errors.description}</p>
-                        )}
-                        <div>
-                          <label>Stock:</label>
-                          <input
-                            className={errors.stock && "danger"}
-                            type="number"
-                            name="stock"
-                            onChange={handleInputChange}
-                            value={input.stock}
-                          />
-                          {errors.stock && (
-                            <p className="danger">{errors.stock}</p>
-                          )}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
-<<<<<<< HEAD
-            <button className="btn1" type="submit">
-              Create!
-            </button>
-          </form>
-        </div>
-=======
             <div>
               <label>Categoría</label>
               <select onChange={(e) => handleCategories(e)}>
@@ -244,8 +162,8 @@ export default function PostProduct() {
             Create!
           </button>
         </form>
->>>>>>> Lighuen
       </div>
+    </div>
     </Layout>
   );
 }
