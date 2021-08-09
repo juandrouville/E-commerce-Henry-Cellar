90d5c88aaebb9d93163300cfa5060d9d6a0e9a6b
lighuen <lighu.miranda@gmail.com>
2021-08-09 13:07:25 -0300

    estilos searchbar y post arreglado

--- client/src/components/productDetail/ProductDetail.js
+++ client/src/components/productDetail/ProductDetail.js
@@ -20,9 +20,9 @@ export default function ProductDetail() {
     };
   }, [dispatch, id]);
 
-  function onClickFiltro() {
-    dispatch(getAllproducts());
-  }
+  // function onClickFiltro() {
+  //   dispatch(getAllproducts());
+  // }
 
   return (
     <div>
