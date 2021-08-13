91ad57ff74da11eced3f7afa93ee7fd5cda9f20e
horaciodemaio <horaciodemaio33@gmail.com>
2021-08-13 16:59:35 -0300

    prueba

--- client/src/index.js
+++ client/src/index.js
@@ -35,10 +35,8 @@ ReactDOM.render(
       <Auth0ProviderWithHistory>
 
         <Route exact path="/prueba" component={Prueba} />
-
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/catalog" component={Home} />
-
         <Route exact path="/create" component={PostProduct} />
         <Route exact path="/product-detail/:id" component={ProductDetail} />
         <Route
