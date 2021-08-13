eb9a3d7bc583aed5aae7f3ef5991226a3e6a72b3
lighuen <lighu.miranda@gmail.com>
2021-08-13 16:53:52 -0300

    client/ searchbar arreglado cuando busca un string vacio

--- client/src/Store/index.js
+++ client/src/Store/index.js
@@ -2,11 +2,11 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "../reducer/index";
 
-const pokemon =
+const wines =
   (typeof window !== "undefined" &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
 
-const Store = createStore(rootReducer, pokemon(applyMiddleware(thunk)));
+const Store = createStore(rootReducer, wines(applyMiddleware(thunk)));
 
 export default Store;
