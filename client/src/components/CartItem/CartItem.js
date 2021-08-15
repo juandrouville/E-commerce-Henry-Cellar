cb1c95ddf03172c0d8fcc012daf0cbcfa3170ff8
Pablo Casals <pcasals76@gmail.com>
2021-08-15 19:25:07 -0300

    algo de estilo al cart

--- client/src/components/CartItem/CartItem.js
+++ client/src/components/CartItem/CartItem.js
@@ -4,11 +4,15 @@ const CartItem = ({ name, price, delFromCart, quantity, id }) => {
 
     return (
         <div>
-            <div>
+            <div className="cartItem">
+                
                 <h3>{name}</h3>
                 <h4>${price} x {quantity} = ${price * quantity}</h4>
-                <button onClick={() => delFromCart(id)}>delet one</button>
-                <button onClick={() => delFromCart(id, true)}>delet all</button>
+                
+                <div className="cartItem_buttons">
+                <button onClick={() => delFromCart(id)} className="cartItem_button">delete one</button>
+                <button onClick={() => delFromCart(id, true)} className="cartItem_button">delete all</button>
+                </div>
             </div>
         </div>
     );
--- client/src/components/cart/Cart.js
+++ client/src/components/cart/Cart.js
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CartItem from "../CartItem/CartItem"
 import { clearCart, removeOneProduct, removeAllProduct, unifyCarts } from "../../actions/index"
-import NavBar from "../NavBar/NavBar";
 import { useAuth0 } from "@auth0/auth0-react";
 import Layout from '../../layouts/layout-primary'
 
@@ -43,13 +42,13 @@ const Cart = () => {
   const { loginWithRedirect } = useAuth0();
   return (
     <Layout>
-      <div>
+      <div className="cart_container">
         <h2>Shopping Cart</h2>
         <div>
           {cart ? (
             cart.map((item, index) => {
               return (
-                <div>
+                <div className="cart_item">
                   <CartItem
                     key={item}
                     id={item.id}
@@ -66,20 +65,20 @@ const Cart = () => {
           )}
         </div>
         <h3>TOTAL:${total}</h3>
-        <div></div>
+        <div >
         <button onClick={(e) => clearcart(e)}>Clear Cart</button>
-      </div>
-      <div>
         {isAuthenticated ? (
           <>
             <button>Buy</button>
           </>
         ) : (
-          <button onClick={() => loginWithRedirect()}>
+          <button onClick={() => loginWithRedirect()} className="buy_button">
             Must Log In to buy
           </button>
         )}
+        </div>
       </div>
+      
     </Layout>
   );
 };
