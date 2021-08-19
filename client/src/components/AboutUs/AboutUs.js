ba84e1f60946a8ec3635dcdb519244dfd28b9758
Pablo Casals <pcasals76@gmail.com>
2021-08-19 16:27:12 -0300

    navbar siempre visible

--- client/src/components/AboutUs/AboutUs.js
+++ client/src/components/AboutUs/AboutUs.js
@@ -7,7 +7,9 @@ export default function AboutUs() {
         <LayoutPrimary>  
             <div className="aboutUs_container">
             <h1>About Us</h1>   
+            <br></br>
             <h2>The Project Members</h2>
+            <br></br>
             <ul>
                 <li>Federico Conte Grand</li>
                 <li>Francisco Lopez</li>
@@ -17,7 +19,9 @@ export default function AboutUs() {
                 <li>Lighuen Miranda</li>
                 <li>Pablo Casals</li>
             </ul>
+            <br></br>
             <h2>The Project</h2>
+            <br></br>
             <p>Build an Ecommerce applying all the skills learned in SoyHenry.com bootcamp. They include React and Redux for the FrontEnd. And Express, Sequelize and PostgreSQL for the BackEnd. 
                 Also investigate and learn how to deploy a project and many other tools in order to accomplish all the features given. </p>
         </div>
