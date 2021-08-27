8dc5c0aa81fc65d8d7d563f0afd84e235ee520bd
lighuen <lighu.miranda@gmail.com>
2021-08-27 03:06:31 -0300

    Merge branch 'main' of https://github.com/Salvapantallado/PF-Vinos-Grupo14 into Lighuen

--- client/src/components/historyUser/historyUser.js
+++ client/src/components/historyUser/historyUser.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import LayoutPrimary from "layouts/layout-primary";
 import { Link, useHistory } from "react-router-dom";
 import { BsXCircle } from "react-icons/bs";
