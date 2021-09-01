01039e5583ed38d3113e5ec333c36e092587f07c
Lighuen Miranda <77689041+Salvapantallado@users.noreply.github.com>
2021-09-01 19:31:12 -0300

    Update LandingPage.js

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -3,7 +3,7 @@ import { ReactComponent as Arrow } from "assets/images/arrow.svg";
 const LandingPage = () => {
   return (
     <div className="landing__container">
-      <h1>we have more than 90 varieties of wines</h1>
+      <h1>Welcome to our wine ecommerce</h1>
       <a href="/home">Check it out!</a>
       <div className="arrow">
         <Arrow />
